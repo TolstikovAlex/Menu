108530b341115b538a492690b24dcd265350b2d6
Alex Tolstikov <tolstikovalexander2@gmail.com>
2022-12-11 06:47:52 +0200

    add form

---
+++ form.js
@@ -0,0 +1,86 @@
+const form = document.querySelector(".menu__form");
+const ingredientName = document.querySelector(".ingredient__name");
+const menuInput = document.querySelector(".menu__input");
+const nameInput = document.querySelector("#name");
+const priceInput = document.querySelector("#price");
+const countInput = document.querySelector("#count");
+const ingredient = document.querySelector(".ingredient");
+
+function createNewItem() {
+  const dish = document.querySelector(".dish");
+  const newEl = `
+  <div class="ingredient">
+  <p class="ingredient__name">${nameInput.value}:</p>
+  <span class="ingredient__count">${countInput.value}(гр./шт.)</span>
+  <span class="ingredient__price">${
+    countInput.value > 50
+      ? priceInput.value * (countInput.value / 1000)
+      : priceInput.value * countInput.value
+  }₴</span>
+  </div>
+  `;
+  const newItem = dish.insertAdjacentHTML("beforeend", newEl);
+  return newItem;
+}
+
+function createNewDish() {
+  const dishes = document.querySelector(".dishes");
+  const newEl = `
+  <div class="dish">
+  <h2 class="dish__title">${menuInput.value}:</h2>
+  </div>`;
+  const newItem = dishes.insertAdjacentHTML("afterbegin", newEl);
+  return newItem;
+}
+
+function onMenuInput(e) {
+  if (menuInput.value && e.key == "Enter") {
+    createNewDish();
+    nameInput.focus();
+    nameInput.value = "";
+  }
+}
+
+function onFocusMenuInput(e) {
+  menuInput.value = "";
+}
+
+function onNameInput(e) {
+  if (nameInput.value && e.key === "Enter") {
+    priceInput.focus();
+    priceInput.value = "";
+  } else if (!nameInput.value && e.key === "Enter") {
+    alert("Введіть, будь ласка, інгредієнт.");
+  }
+}
+
+function onPriceInput(e) {
+  const isValidValue = !isNaN(e.target.value) && e.target.value;
+  if (isValidValue && e.key === "Enter") {
+    countInput.focus();
+    countInput.value = "";
+  } else if (!isValidValue && e.key === "Enter") {
+    alert("Введіть, будь ласка, вартість.");
+  }
+}
+
+function onCountInput(e) {
+  const isValidValue = !isNaN(e.target.value) && e.target.value;
+  if (isValidValue && e.key === "Enter") {
+    createNewItem();
+    nameInput.focus();
+    nameInput.value = "";
+    priceInput.value = "0";
+    countInput.value = "0";
+  } else if (!isValidValue && e.key === "Enter") {
+    alert("Введіть, будь ласка, кількість.");
+  }
+  if (countInput.value && e.key == "Enter") {
+  }
+}
+
+menuInput.addEventListener("keydown", onMenuInput);
+menuInput.addEventListener("focus", onFocusMenuInput);
+nameInput.addEventListener("keydown", onNameInput);
+priceInput.addEventListener("keydown", onPriceInput);
+countInput.addEventListener("keydown", onCountInput);
--- index.js
+++ index.js
@@ -1,45 +1,67 @@
 const menuInput = document.querySelector(".menu__input");
 const menuTitle = document.querySelector(".menu__title");
 const dishTitle = document.querySelector(".dish__title");
-const recipeItem = document.querySelector(".recipe__name");
-const recipeTitle = document.querySelector(".recipe__title");
+const addBtn = document.querySelector(".ingredient__btn");
 const ingredientName = document.querySelector("#name");
 const price = document.querySelector("#price");
 const count = document.querySelector("#count");
 const totalPrice = document.querySelector("#total-price");
-const recipeCount = document.querySelector(".recipe__count");
 const ingredientList = document.querySelector(".ingredients");
+const ingredients = document.querySelector(".ingredients__list");
 
+//блюдо
 function onMenuInput(e) {
-  if (e.key === "Enter") {
-    dishTitle.textContent = menuInput.value + ":";
-    recipeTitle.textContent = menuInput.value + ":";
-    menuInput.value = "";
+  const recipe = document.querySelector(".recipe");
+  const recipeTitle = document.querySelector(".recipe__title");
+  const wrapper = document.querySelector(".recipe__wrapper");
+  const newEl = ` <div class="recipe__wrapper">
+  <h3 class="recipe__title"></h3>
+            </div>`;
+  recipe.insertAdjacentHTML("beforeend", newEl);
+  if (menuInput.value && e.key === "Enter") {
+    // dishTitle.textContent = `${menuInput.value}:`;
+    recipeTitle.textContent = `${menuInput.value}:`;
+    // menuInput.value = "";
+    ingredientName.focus();
+  } else if (!menuInput.value && e.key === "Enter") {
+    alert("Введіть, будь ласка, назву страви.");
   }
 }
 
+//ингредиент
 function onIngredientName(e) {
-  if (e.key === "Enter") {
-    recipeItem.textContent = `${ingredientName.value}:`;
+  if (ingredientName.value && e.key === "Enter") {
     price.focus();
+    price.value = "";
+  } else if (!ingredientName.value && e.key === "Enter") {
+    alert("Введіть, будь ласка, інгредієнт.");
   }
 }
 
+//цена
 function onPriceInput(e) {
-  if (e.key === "Enter") {
-    if (+parseFloat(count.value) >= 50) {
-      totalPrice.value =
-        +parseFloat(price.value) * (+parseFloat(count.value) / 1000) + "₴";
-    } else {
-      totalPrice.value =
-        +parseFloat(price.value) * +parseFloat(count.value) + "₴";
-    }
+  const isValidPrice = !isNaN(price.value) && price.value;
+  if (isValidPrice && e.key === "Enter") {
     count.focus();
+    count.value = "";
+  } else if (!isValidPrice && e.key === "Enter") {
+    alert("Введіть, будь ласка, вартість.");
   }
 }
 
+//количество
 function onCountInput(e) {
-  if (e.key === "Enter") {
+  const newItem = `<div class="recipe__item">
+  <p class="recipe__name"></p>
+  <span class="recipe__count"></span>
+  <span class="recipe__price"></span>
+  </div>`;
+  const wrapper = document.querySelector(".recipe__wrapper");
+  const recipeItem = document.querySelector(".recipe__name");
+  const recipeCount = document.querySelector(".recipe__count");
+  const recipePrice = document.querySelector(".recipe__price");
+  const isValidCount = !isNaN(count.value) && count.value;
+  if (isValidCount && e.key === "Enter") {
     if (+parseFloat(count.value) >= 50) {
       totalPrice.value =
         +parseFloat(price.value) * (+parseFloat(count.value) / 1000) + "₴";
@@ -47,11 +69,36 @@ function onCountInput(e) {
       totalPrice.value =
         +parseFloat(price.value) * +parseFloat(count.value) + "₴";
     }
-    recipeCount.textContent = count.value;
+    wrapper.insertAdjacentHTML("afterend", newItem);
+    recipeItem.textContent = `${ingredientName.value}:`;
+    recipeCount.textContent = `${count.value}(гр./шт.)`;
+    recipePrice.textContent = totalPrice.value;
+    console.log(recipeItem.textContent);
+    count.blur();
+    ingredientName.value = "";
+    price.value = "";
+    count.value = "";
+    totalPrice.value = "";
+    // menuInput.value = "";
+    // addBtn.focus();
+    ingredientList.classList.add("invisible");
+    addBtn.classList.remove("invisible");
+  } else if (!isValidCount && e.key === "Enter") {
+    alert("Введіть, будь ласка, кількість.");
   }
 }
 
-ingredientName.addEventListener("keydown", onIngredientName);
+//сабмит
+function onBtnClick(e) {
+  ingredientList.classList.remove("invisible");
+  addBtn.classList.add("invisible");
+  ingredientName.focus();
+}
+
+//слушатели
+
 count.addEventListener("keydown", onCountInput);
 price.addEventListener("keydown", onPriceInput);
+ingredientName.addEventListener("keydown", onIngredientName);
 menuInput.addEventListener("keydown", onMenuInput);
+addBtn.addEventListener("click", onBtnClick);
